141ebb5abb361f3a8c5301b7680e1ab47bda939e
ohardin <obie@obie.net>
2023-02-28 17:00:59 -0600

    Update main.go

---
+++ new_mod/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, You!"
+}
+
+func main() {
+	fmt.Println("Hello, Main!")
+
+}
--- new_mod/new_mod.go
+++
@@ -1,5 +0,0 @@
-package new_mod
-
-func Hello() string {
-	return "Hello, You!"
-}
